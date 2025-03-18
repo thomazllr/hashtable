708831852bd3053ef9f20545726e76746dd4afc0
thomazllr <guithomaz270@gmail.com>
2025-03-18 11:58:59 -0300

    add: gerador de hashtables

---
+++ src/gerador/GeradorChaves.java
@@ -0,0 +1,42 @@
+package gerador;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class GeradorChaves {
+
+    public static Set<String> gerarListaChaves(int quantidade) {
+        Set<String> chaves = new HashSet<>();
+        Random random = new Random();
+
+        while (chaves.size() < quantidade) {
+            int chave = 100_000_000 + random.nextInt(900_000_000);
+            chaves.add(String.valueOf(chave));
+        }
+
+        return chaves;
+    }
+
+    public static void salvarEmArquivo(Set<String> chaves, String nomeArquivo) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(nomeArquivo))) {
+            for (String chave : chaves) {
+                writer.write(chave);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.err.println("Erro ao salvar o arquivo: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        int quantidade = 1000000;  // Quantidade de chaves a serem geradas
+        Set<String> listaChaves = gerarListaChaves(quantidade);
+        salvarEmArquivo(listaChaves, "chaves.txt");
+
+        System.out.println(quantidade + " chaves geradas e salvas em 'chaves.txt'.");
+    }
+}
